44ce8a53d40548df7e739f89705e0d884a0794c1
p-deathstroke <preetdabhi14@gmail.com>
2022-01-05 14:22:36 -0500

    added the proper coordinates of the pos

--- src/mqtt/subscriber.js
+++ src/mqtt/subscriber.js
@@ -2,7 +2,9 @@ const mqtt = require("async-mqtt");
 var fs = require("fs");
 // let connectUrl = `mqtt.eclipseprojects.io:1883`; //previos connectUrl
 //let connectUrl = `mqtt://broker.emqx.io:1883`; // working connectUrl
-let connectUrl = `mqtt://155.246.223.11:1883`; // ip address connectUrl
+let connectUrl = `mqtt://192.168.0.100:1883`; // ip address connectUrl
+//192.168.0.100
+//155.246.223.11
 
 // Connecting with the publisher
 const client = mqtt.connect(connectUrl, {
@@ -19,21 +21,24 @@ const client = mqtt.connect(connectUrl, {
   password:"", // ip address connectUrl
   reconnectPeriod: 1000,
 });
+//console.log(client)
 // 155.246.223.11
 client.on("connect", function () {
   // louislidar3
-  client.subscribe("louislidar3", () => {
+  client.subscribe("louis_lidar3", () => {
     console.log("Client has subscribed successfully.");
   });
 });
 
 // Receiving the message and converting it into json format
 client.on("message", function (topic, message) {
+  
+let coordinates = message.toString();
+let coordinatesArr = coordinates.split(',')
   console.log("Connected");
-
   let robotData_json = {
-    Robot_X_Position: message[0].toString(),
-    Robot_Y_Position: message[1].toString(),
+    Robot_X_Position: coordinatesArr[0].toString(),
+    Robot_Y_Position: coordinatesArr[1].toString(),
   };
   console.log(JSON.stringify(robotData_json));
 
