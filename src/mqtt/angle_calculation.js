fdfc5b0a140a9f9cc995d63339e375be36aac667
rajpaynaik <rajpaynaik18@gmail.com>
2022-01-11 14:37:19 -0500

    initial commint

--- src/mqtt/angle_calculation.js
+++ src/mqtt/angle_calculation.js
@@ -1,15 +1,26 @@
 var fs = require("fs");
 let RobotFile = fs.readFileSync("robot_data.json", "utf-8");
-// let distanceFile = fs.readFileSync("distance.json", "utf-8");
+let angleFile = fs.readFileSync("angle.json", "utf-8");
 let parseRoboFile = JSON.parse(RobotFile);
-let slope = [];
+let parseAngleFile = JSON.parse(angleFile);
 
 for (let i = 0; i < parseRoboFile.length - 1; i++) {
-  let slopeCal =
-    (parseRoboFile[i + 1].Robot_Y_Position -
-      parseRoboFile[i].Robot_Y_Position) /
-    (parseRoboFile[i + 1].Robot_X_Position - parseRoboFile[i].Robot_X_Position);
-}
-//   parseDistanceFile.push(distanceCal);
+  let dy =
+    parseRoboFile[i + 1].Robot_Y_Position - parseRoboFile[i].Robot_Y_Position;
+  let dx =
+    parseRoboFile[i + 1].Robot_X_Position - parseRoboFile[i].Robot_X_Position;
 
-// fs.writeFileSync("distance.json", JSON.stringify(parseDistanceFile), "utf-8");
+  let thetha = Math.atan2(dy, dx);
+
+  thetha *= 180 / Math.PI;
+
+  let angleCal = {
+    seconds: (i + 1) * 20,
+    RobotX: parseRoboFile[i].Robot_X_Position,
+    RobotY: parseRoboFile[i].Robot_Y_Position,
+    angle: thetha,
+  };
+  parseAngleFile.push(angleCal);
+}
+
+fs.writeFileSync("angle.json", JSON.stringify(parseAngleFile), "utf-8");
